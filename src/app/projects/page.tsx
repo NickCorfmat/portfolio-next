4f16d3e2e13fd0da96d8002ad2c6025f6272b9d1
Nick Corfmat <ncorfmat@ucsc.edu>
2026-04-15 17:46:31 -0700

    added project grid

--- src/app/projects/page.tsx
+++ src/app/projects/page.tsx
@@ -1,7 +1,33 @@
+"use client";
+import ProjectCard from "@components/ProjectCard";
+import ProjectPopup from "@/src/components/ProjectPopup";
+import { Project } from "@/public/data/projects";
 import { getProjectsByCategory } from "@/src/lib/projectUtils";
+import { useState } from "react";
 
 export default function ProjectPage() {
-  const allProjects = getProjectsByCategory(["game", "program"]);
+  const [activeProject, setActiveProject] = useState<Project | null>(null);
+  const projects = getProjectsByCategory(["game", "program"]);
 
-  return <div></div>;
+  return (
+    <section className="w-full py-24 flex flex-col items-center px-6">
+      <div className="grid gap-12 w-full justify-center [grid-template-columns:repeat(auto-fill,400px)]">
+        {projects.map((project) => (
+          <ProjectCard
+            key={project.name}
+            project={project}
+            onClick={() => setActiveProject(project)}
+            className="w-[400px] aspect-[400/260]"
+          />
+        ))}
+      </div>
+
+      {activeProject && (
+        <ProjectPopup
+          project={activeProject}
+          onClose={() => setActiveProject(null)}
+        />
+      )}
+    </section>
+  );
 }
