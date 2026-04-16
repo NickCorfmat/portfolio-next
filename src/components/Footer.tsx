e29ec016b2427330a9718323a534fa8c32eb5d35
Nick Corfmat <ncorfmat@ucsc.edu>
2026-04-15 17:47:39 -0700

    added external links

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -4,19 +4,24 @@ import { NAVBAR_ITEMS } from "@lib/constants";
 
 export function Footer() {
   return (
-    <div className="w-full h-[150px] flex flex-row justify-center items-center bg-black text-white gap-8">
-      <div className="flex flex-row gap-8">
+    <div className="w-full h-[150px] flex flex-row justify-center items-center bg-black text-white text-sm gap-24">
+      <div className="flex flex-row gap-12">
         {NAVBAR_ITEMS.map((item) => (
-          <div>
-            <Link key={item.label} href={item.href}>
-              {item.label.toUpperCase()}
-            </Link>
-          </div>
+          <Link key={item.label} href={item.href}>
+            {item.label.toUpperCase()}
+          </Link>
         ))}
       </div>
-      <div className="text-2xl">
-        <i className="devicon-github-original"></i>
-        <i className="devicon-linkedin-plain"></i>
+      <div className="flex flex-row gap-6 text-xl">
+        <a href="https://www.linkedin.com/in/nicolascorfmat/">
+          <i className="devicon-github-original"></i>
+        </a>
+        <a href="https://github.com/NickCorfmat">
+          <i className="devicon-linkedin-plain"></i>
+        </a>
+        <a href="mailto:nfcorfmat@gmail.com">
+          <i className="fa-regular fa-envelope"></i>
+        </a>
       </div>
     </div>
   );
