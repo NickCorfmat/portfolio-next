77d5bcc01e780272e1a132362071ce4827d87036
Nick Corfmat <ncorfmat@ucsc.edu>
2026-04-16 20:50:55 -0700

    added project randomizer helper

--- src/lib/projectUtils.ts
+++ src/lib/projectUtils.ts
@@ -14,3 +14,12 @@ export const getRandomProject = (projects: Project[]) => {
 export const isVideo = (media: string) => {
   return media.endsWith(".mp4") || media.endsWith(".webm");
 };
+
+export function shuffleProjects(array: Project[]) {
+  const arr = [...array];
+  for (let i = arr.length - 1; i > 0; i--) {
+    const j = Math.floor(Math.random() * (i + 1));
+    [arr[i], arr[j]] = [arr[j], arr[i]];
+  }
+  return arr;
+}
